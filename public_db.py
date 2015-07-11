85d116c84b8ed39fb5a494f03376307d91312078
bigzhu <vermiliondun@gmail.com>
2015-07-11 23:10:47 +0800

    按照twitter的时间来排序

--- public_db.py
+++ public_db.py
@@ -4,7 +4,7 @@ import pg
 def getTwitterMessages():
     sql = '''
     select * from twitter_message tm, twitter_user tu
-        where tm.t_user_id=tu.id_str
+        where tm.t_user_id=tu.id_str order by tm.created_at desc
     '''
     return pg.db.query(sql)
 
