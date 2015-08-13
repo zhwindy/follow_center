827b83de0c18d92965b7b9a6588f7bef1eeb7673
bigzhu <vermiliondun@gmail.com>
2015-08-13 18:45:59 +0800

    无辜的代码恢复

--- public_db.py
+++ public_db.py
@@ -120,8 +120,7 @@ def getMessages(user_id=None, god_name=None, type=None, id=None, limit=None, off
         ''' % (sql, limit)
     if offset:
         sql = sql + ' offset %s' % offset
-    #sql = "SET statement_timeout = '2s';"+sql
-    return pg.db.query(sql)
+    return pg.query(sql)
 
 
 def getGodInfoFollow(user_id=None, god_name=None):
